cebe32cbf090e4a380984a4cc51ad226c85e9785
Jordi Torrents <jordi.t21@gmail.com>
2013-08-15 19:02:05 +0200

    Add Moody and Newman approximation for local node connectivity and friends.

---
+++ networkx/algorithms/connectivity/approximation/__init__.py
@@ -0,0 +1,4 @@
+"""Module for approximation algorithms to connectivity and cut
+"""
+from networkx.algorithms.connectivity.approximation.connectivity import *
+#from networkx.algorithms.connectivity.approximation.kcomponents import *
