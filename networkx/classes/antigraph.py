af798f2efb65de99a902892303ecaa01b2685641
Jordi Torrents <jordi.t21@gmail.com>
2013-08-15 19:09:34 +0200

    Add AntiGraph, a complement graph class.
    This is a dependency for the approximation to k-components

---
+++ networkx/classes/antigraph.py
@@ -0,0 +1,272 @@
+""" Class for complement graphs.
+
+"""
+
+from copy import deepcopy
+import networkx as nx
+from networkx.exception import NetworkXError
+import networkx.convert as convert
+
+class AntiGraph(nx.Graph):
+    """
+    Class for complement graphs.
+    """
+    def __init__(self, data=None, **attr):
+        """Initialize a graph with edges, name, graph attributes.
+
+        Parameters
+        ----------
+        data : input graph
+            Data to initialize graph.  If data=None (default) an empty
+            graph is created.  The data can be an edge list, or any
+            NetworkX graph object.  If the corresponding optional Python
+            packages are installed the data can also be a NumPy matrix
+            or 2d ndarray, a SciPy sparse matrix, or a PyGraphviz graph.
+        name : string, optional (default='')
+            An optional name for the graph.
+        attr : keyword arguments, optional (default= no attributes)
+            Attributes to add to graph as key=value pairs.
+
+        See Also
+        --------
+        convert
+
+        Examples
+        --------
+        >>> G = nx.Graph()   # or DiGraph, MultiGraph, MultiDiGraph, etc
+        >>> G = nx.Graph(name='my graph')
+        >>> e = [(1,2),(2,3),(3,4)] # list of edges
+        >>> G = nx.Graph(e)
+
+        Arbitrary graph attribute pairs (key=value) may be assigned
+
+        >>> G=nx.Graph(e, day="Friday")
+        >>> G.graph
+        {'day': 'Friday'}
+
+        """
+        self.graph = {}   # dictionary for graph attributes
+        self.node = {}    # empty node dict (created before convert)
+        self.adj = {}     # empty adjacency dict
+        # attempt to load graph with data
+        if data is not None:
+            convert.to_networkx_graph(data,create_using=self)
+        # load graph attributes (must be after convert)
+        self.graph.update(attr)
+        self.edge = self.adj
+
+
+    def __getitem__(self, n):
+        """Return a dict of neighbors of node n.  Use the expression 'G[n]'.
+
+        Parameters
+        ----------
+        n : node
+           A node in the graph.
+
+        Returns
+        -------
+        adj_dict : dictionary
+           The adjacency dictionary for nodes connected to n.
+
+        Notes
+        -----
+        G[n] is similar to G.neighbors(n) but the internal data dictionary
+        is returned instead of a list.
+
+        Assigning G[n] will corrupt the internal graph data structure.
+        Use G[n] for reading data only.
+
+        Examples
+        --------
+        >>> G = nx.Graph()   # or DiGraph, MultiGraph, MultiDiGraph, etc
+        >>> G.add_path([0,1,2,3])
+        >>> G[0]
+        {1: {}}
+        """
+        return dict((node,{}) for node in 
+                        set(self.adj)-set(self.adj[n])-set([n]))
+
+
+    def neighbors(self, n):
+        """Return a list of the nodes connected to the node n.
+
+        Parameters
+        ----------
+        n : node
+           A node in the graph
+
+        Returns
+        -------
+        nlist : list
+            A list of nodes that are adjacent to n.
+
+        Raises
+        ------
+        NetworkXError
+            If the node n is not in the graph.
+
+        Notes
+        -----
+        It is usually more convenient (and faster) to access the
+        adjacency dictionary as G[n]:
+
+        >>> G = nx.Graph()   # or DiGraph, MultiGraph, MultiDiGraph, etc
+        >>> G.add_edge('a','b',weight=7)
+        >>> G['a']
+        {'b': {'weight': 7}}
+
+        Examples
+        --------
+        >>> G = nx.Graph()   # or DiGraph, MultiGraph, MultiDiGraph, etc
+        >>> G.add_path([0,1,2,3])
+        >>> G.neighbors(0)
+        [1]
+
+        """
+        try:
+            return list(set(self.adj)-set(self.adj[n])-set([n]))
+        except KeyError:
+            raise NetworkXError("The node %s is not in the graph."%(n,))
+
+    def neighbors_iter(self, n):
+        """Return an iterator over all neighbors of node n.
+
+        Examples
+        --------
+        >>> G = nx.Graph()   # or DiGraph, MultiGraph, MultiDiGraph, etc
+        >>> G.add_path([0,1,2,3])
+        >>> [n for n in G.neighbors_iter(0)]
+        [1]
+
+        Notes
+        -----
+        It is faster to use the idiom "in G[0]", e.g.
+
+        >>> G = nx.path_graph(4)
+        >>> [n for n in G[0]]
+        [1]
+        """
+        try:
+            return iter(set(self.adj)-set(self.adj[n])-set([n]))
+        except KeyError:
+            raise NetworkXError("The node %s is not in the graph."%(n,))
+
+
+    def degree(self, nbunch=None, weight=None):
+        """Return the degree of a node or nodes.
+
+        The node degree is the number of edges adjacent to that node.
+
+        Parameters
+        ----------
+        nbunch : iterable container, optional (default=all nodes)
+            A container of nodes.  The container will be iterated
+            through once.
+
+        weight : string or None, optional (default=None)
+           The edge attribute that holds the numerical value used 
+           as a weight.  If None, then each edge has weight 1.
+           The degree is the sum of the edge weights adjacent to the node.
+
+        Returns
+        -------
+        nd : dictionary, or number
+            A dictionary with nodes as keys and degree as values or
+            a number if a single node is specified.
+
+        Examples
+        --------
+        >>> G = nx.Graph()   # or DiGraph, MultiGraph, MultiDiGraph, etc
+        >>> G.add_path([0,1,2,3])
+        >>> G.degree(0)
+        1
+        >>> G.degree([0,1])
+        {0: 1, 1: 2}
+        >>> list(G.degree([0,1]).values())
+        [1, 2]
+
+        """
+        if nbunch in self:      # return a single node
+            return next(self.degree_iter(nbunch,weight))[1]
+        else:           # return a dict
+            return dict(self.degree_iter(nbunch,weight))
+
+    def degree_iter(self, nbunch=None, weight=None):
+        """Return an iterator for (node, degree).
+
+        The node degree is the number of edges adjacent to the node.
+
+        Parameters
+        ----------
+        nbunch : iterable container, optional (default=all nodes)
+            A container of nodes.  The container will be iterated
+            through once.
+
+        weight : string or None, optional (default=None)
+           The edge attribute that holds the numerical value used 
+           as a weight.  If None, then each edge has weight 1.
+           The degree is the sum of the edge weights adjacent to the node.
+
+        Returns
+        -------
+        nd_iter : an iterator
+            The iterator returns two-tuples of (node, degree).
+
+        See Also
+        --------
+        degree
+
+        Examples
+        --------
+        >>> G = nx.Graph()   # or DiGraph, MultiGraph, MultiDiGraph, etc
+        >>> G.add_path([0,1,2,3])
+        >>> list(G.degree_iter(0)) # node 0 with degree 1
+        [(0, 1)]
+        >>> list(G.degree_iter([0,1]))
+        [(0, 1), (1, 2)]
+
+        """
+        if nbunch is None:
+            nodes_nbrs = ((n,list(set(self.adj)-set(self.adj[n])-set([n])))
+                            for n in self.nodes_iter())
+        else:
+            nodes_nbrs= ((n,list(set(self.nodes())-set(self.adj[n])-set([n])))
+                            for n in self.nbunch_iter(nbunch))
+
+        if weight is None:
+            for n,nbrs in nodes_nbrs:
+                yield (n,len(nbrs)+(n in nbrs)) # return tuple (n,degree)
+        else:
+        # Weighted degree not implemented for AntiGraph
+            for n,nbrs in nodes_nbrs:
+                yield (n, sum((nbrs[nbr].get(weight,1) for nbr in nbrs)) +
+                              (n in nbrs and nbrs[n].get(weight,1)))
+
+    def adjacency_iter(self):
+        """Return an iterator of (node, adjacency dict) tuples for all nodes.
+
+        This is the fastest way to look at every edge.
+        For directed graphs, only outgoing adjacencies are included.
+
+        Returns
+        -------
+        adj_iter : iterator
+           An iterator of (node, adjacency dictionary) for all nodes in
+           the graph.
+
+        See Also
+        --------
+        adjacency_list
+
+        Examples
+        --------
+        >>> G = nx.Graph()   # or DiGraph, MultiGraph, MultiDiGraph, etc
+        >>> G.add_path([0,1,2,3])
+        >>> [(n,nbrdict) for n,nbrdict in G.adjacency_iter()]
+        [(0, {1: {}}), (1, {0: {}, 2: {}}), (2, {1: {}, 3: {}}), (3, {2: {}})]
+
+        """
+        for n in self.adj:
+            yield (n, set(self.adj)-set(self.adj[n])-set([n]))
+        #return iter(self.adj.items())
